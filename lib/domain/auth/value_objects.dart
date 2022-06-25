2e510e51d588fe749c13c8f89d6ce17b5135e1a1
Alex Martynov <martynov.alex@gmail.com>
2022-06-25 18:09:24 +0600

    [2] – Authentication Value Objects

---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:notes_firebase_ddd_course/domain/core/failures.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_object.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
+
+// void showingTheEmailAddressOrFailure() {
+//   final emailAddress = EmailAddress('fsfdss'); // Возвращает Either<ValueFailure<String>, String>
+//
+//   // Первый вариант работы с Either
+//   String emailText1 = emailAddress.value.fold(
+//     (left) => 'Failure happened, more precisely: $left',
+//     (right) => right,
+//   );
+//
+//   // Второй вариант работы с Either
+//   String emailText2 = emailAddress.value.getOrElse(() => 'Some failure happened');
+// }
