03d6ddea53f6125e15b07eec08fd729f02937088
Alex Martynov <martynov.alex@gmail.com>
2022-06-28 15:26:41 +0600

    [4] – Modeling the Sign-In Form Events & State

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_firebase_ddd_course/domain/auth/auth_failure.dart';
+import 'package:notes_firebase_ddd_course/domain/auth/i_auth_facade.dart';
+import 'package:notes_firebase_ddd_course/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_bloc.freezed.dart';
+part 'sign_in_form_state.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade) : super(SignInFormState.initial()) {
+    on<SignInFormEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
